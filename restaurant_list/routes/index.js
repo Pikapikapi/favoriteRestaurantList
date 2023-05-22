8d9eb55bbeb300b9e44e0209693e835f5f880b54
Pikapikapi <huang.paladin@gmail.com>
2023-05-22 13:09:18 +0800

    feat : add user modules

--- restaurant_list/routes/index.js
+++ restaurant_list/routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurants')
+const user = require('./modules/users')
 
+router.use('/users', user)
 router.use('/', home)
 router.use('/restaurants', restaurant)
+
 module.exports = router
