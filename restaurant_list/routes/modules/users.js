fdc1a021613d58172f5f176ecb9ec226f6315457
Pikapikapi <huang.paladin@gmail.com>
2023-05-23 13:19:22 +0800

    feat: add bcrypt.js

--- restaurant_list/routes/modules/users.js
+++ restaurant_list/routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 //login block
 router.get('/login', (req, res) => {
@@ -33,32 +34,33 @@ router.post('/register', (req, res) => {
     errors.push({ message: '密碼與確認密碼不相符！' })
   }
   // 檢查使用者是否已經註冊
-  User.findOne({ email })
-    .then((user) => {
-      // 如果已經註冊：退回原本畫面
-      if (user) {
-        errors.push({ message: '這個 Email 已經註冊過了。' })
-        res.render('register', {
+  User.findOne({ email }).then((user) => {
+    // 如果已經註冊：退回原本畫面
+    if (user) {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword,
+      })
+    }
+    return bcrypt
+      .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+      .then((salt) => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+      .then((hash) =>
+        User.create({
           name,
           email,
-          password,
-          confirmPassword,
+          password: hash, // 用雜湊值取代原本的使用者密碼
         })
-      } else {
-        // 如果還沒註冊：寫入資料庫
-        return User.create({
-          name: name || 'user',
-          email,
-          password,
-        })
-          .then(() => {
-            req.flash('success_msg', '帳號註冊成功。')
-            res.redirect('/')
-          })
-          .catch((err) => console.log(err))
-      }
-    })
-    .catch((err) => console.log(err))
+      )
+      .then(() => {
+        req.flash('success_msg', '帳號註冊成功。')
+        res.redirect('/')
+      })
+      .catch((err) => console.log(err))
+  })
 })
 //register block end
 
